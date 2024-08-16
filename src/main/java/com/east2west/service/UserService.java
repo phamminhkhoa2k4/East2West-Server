ac0ee59c7e9ea512a06a9bb5222c0b834e8086df
pham minh khoa <khoapham280@gmail.com>
2024-08-16 11:22:46 +0700

    feat:list user

---
+++ src/main/java/com/east2west/service/UserService.java
@@ -0,0 +1,17 @@
+package com.east2west.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.east2west.models.Entity.User;
+import com.east2west.repository.*;
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
