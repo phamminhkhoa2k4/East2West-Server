744e41708db698be449b31908f159d903b618dc7
pham minh khoa <khoapham280@gmail.com>
2024-08-16 11:42:14 +0700

    feat: addition make , type , and model .

---
+++ src/main/java/com/east2west/models/Entity/Type.java
@@ -0,0 +1,40 @@
+package com.east2west.models.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
+@Table(name = "Types")
+public class Type {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "typeid")
+    private int typeId;
+
+    public int getTypeId() {
+        return this.typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    @Column(name = "typename")
+    private String typeName;
+
+    public String getTypeName() {
+        return this.typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    // Constructors, getters, setters, etc.
+}
