b34a5f4c7fb314e1eec7cb313e509195c8a43abf
pham minh khoa <khoapham280@gmail.com>
2024-09-03 23:14:20 +0700

    fix bug not run conflict

--- src/main/java/com/east2west/util/StringListConverter.java
+++ src/main/java/com/east2west/util/StringListConverter.java
@@ -4,25 +4,26 @@ import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Converter
-public class StringListConverter implements AttributeConverter<List<String>, String[]> {
+public class StringListConverter implements AttributeConverter<List<String>, String> {
 
     @Override
-    public String[] convertToDatabaseColumn(List<String> attribute) {
+    public String convertToDatabaseColumn(List<String> attribute) {
         if (attribute == null || attribute.isEmpty()) {
-            return new String[0];
+            return "";
         }
-        return attribute.toArray(new String[0]);
+        return String.join(",", attribute);
     }
 
     @Override
-    public List<String> convertToEntityAttribute(String[] dbData) {
-        if (dbData == null || dbData.length == 0) {
-            return null;
+    public List<String> convertToEntityAttribute(String dbData) {
+        if (dbData == null || dbData.isEmpty()) {
+            return List.of();
         }
-        return Arrays.asList(dbData);
+        return Arrays.asList(dbData.split(","));
     }
 }
+
 
