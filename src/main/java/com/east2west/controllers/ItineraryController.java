c3b5f90b96065a0cc4d3e43510157970aae6f09d
pham minh khoa <khoapham280@gmail.com>
2024-09-06 11:02:37 +0700

    resolved error

--- src/main/java/com/east2west/controllers/ItineraryController.java
+++ src/main/java/com/east2west/controllers/ItineraryController.java
@@ -36,17 +36,17 @@ public class ItineraryController {
     public ResponseEntity<Itinerary> createOrUpdateItinerary(@RequestBody ItineraryDTO itineraryDTO) {
         Itinerary itinerary = itineraryService.createOrUpdateItinerary(itineraryDTO);
         return ResponseEntity.ok(itinerary);
-    }   
+    }
     // {
-    //     "itineraryId": null,  // Optional, use if updating
-    //tourPackageId:1
-    //     "name": "Northern Lights Tour",
-    //     "description": "A 5-day tour to explore the Northern Lights",
-    //     "accommodationIds": [1, 2],  // List of accommodation IDs
-    //     "mealIds": [1, 3],  // List of meal IDs
-    //     "placeIds": [4, 5]  // List of place IDs
+    // "itineraryId": null, // Optional, use if updating
+    // tourPackageId:1
+    // "name": "Northern Lights Tour",
+    // "description": "A 5-day tour to explore the Northern Lights",
+    // "accommodationIds": [1, 2], // List of accommodation IDs
+    // "mealIds": [1, 3], // List of meal IDs
+    // "placeIds": [4, 5] // List of place IDs
     // }
-    
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteItinerary(@PathVariable int id) {
         itineraryService.deleteItinerary(id);
@@ -67,8 +67,12 @@ public class ItineraryController {
     }
 
     @GetMapping("/accommodations/search")
-    public ResponseEntity<List<Accommodation>> searchAccommodations(@RequestParam(required = false, defaultValue = "") String search) {
-        List<Accommodation> accommodations = itineraryService.searchAccommodationsByName(search);
+    public ResponseEntity<List<Accommodation>> searchAccommodations(@RequestParam String search) {
+        List<Accommodation> accommodations = null;
+        if (search != "") {
+            accommodations = itineraryService.searchAccommodationsByName(search);
+        }
+
         return ResponseEntity.ok(accommodations);
     }
 
@@ -92,8 +96,11 @@ public class ItineraryController {
     }
 
     @GetMapping("/places/search")
-    public ResponseEntity<List<Place>> searchPlaces(@RequestParam(required = false, defaultValue = "") String search) {
-        List<Place> places = itineraryService.searchPlacesByName(search);
+    public ResponseEntity<List<Place>> searchPlaces(@RequestParam String search) {
+        List<Place> places =null;
+        if(search!=""){
+            places= itineraryService.searchPlacesByName(search);
+        }
         return ResponseEntity.ok(places);
     }
 
@@ -117,8 +124,11 @@ public class ItineraryController {
     }
 
     @GetMapping("/meals/search")
-    public ResponseEntity<List<Meal>> searchMeals(@RequestParam(required = false, defaultValue = "") String search) {
-        List<Meal> meals = itineraryService.searchMealsByName(search);
+    public ResponseEntity<List<Meal>> searchMeals(@RequestParam String search) {
+        List<Meal> meals = null;
+        if(search!=""){
+            meals=itineraryService.searchMealsByName(search);
+        }
         return ResponseEntity.ok(meals);
     }
 
@@ -142,8 +152,11 @@ public class ItineraryController {
     }
 
     @GetMapping("/transfers/search")
-    public ResponseEntity<List<Transfer>> searchTransfers(@RequestParam(required = false, defaultValue = "") String search) {
-        List<Transfer> transfers = itineraryService.searchTransfersByName(search);
+    public ResponseEntity<List<Transfer>> searchTransfers(@RequestParam String search) {
+        List<Transfer> transfers = null;
+        if(search!=null){
+            transfers=itineraryService.searchTransfersByName(search);
+        }
         return ResponseEntity.ok(transfers);
     }
 
